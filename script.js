b2e4b5a5cde2b6349bb15385538e974023fef8fb
ryanguyen00 <ryan.c.nguyen@bellevuecollege.edu>
2022-02-04 18:31:08 -0800

    phase 1

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("hello");
+
+
+document.addEventListener("DOMContentLoaded", function () {
+   
+   
+ 
+  
+document.getElementById("addButton").addEventListener("click", function () {
+  
+   console.log(document.getElementById("price").value);
+        
+  
+});
+
+
+});
