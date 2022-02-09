84309f8976954812711a233a4dcba9970dd822e2
ryanguyen00 <ryan.c.nguyen@bellevuecollege.edu>
2022-02-08 21:48:43 -0800

    changed js to include array and put values into array

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-console.log("hello");
+let shoeArray = [];
+
+let shoeObject = function (aShoeName, aYear, aPrice, aURL ) {
+   this.ID = Math.random().toString(16).slice(5)
+   this.shoeName = aShoeName,
+   this.year = aYear,
+   this.price = aPrice;
+   this.url = aURL;
+}
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
    
    
- 
+   document.getElementById("addButton").addEventListener("click", function () {
+      shoeArray.push(new shoeObject(document.getElementById("shoeName").value, document.getElementById("year").value,
+         document.getElementById("price").value, document.getElementById("URL").value));
+     
+      console.log(shoeArray);
+      
+  });
+
+
   
-document.getElementById("addButton").addEventListener("click", function () {
-  
-   console.log(document.getElementById("price").value);
-        
-  
-});
+
 
 
 });
